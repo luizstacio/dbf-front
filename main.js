155807bf7421bdd299752350410db71d2446fc62
Luiz Estácio <luizstacio@gmail.com>
2020-01-28 14:58:16 -0300

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+import parsedbf from 'parsedbf';
+import { Buffer } from 'buffer';
+
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+document.querySelector('#myfile').addEventListener('change', () => {
+    const file = document.querySelector('#myfile').files[0];
+    toBase64(file).then(data => {
+        const buffer = Buffer.from(data);
+        const parsedDBF = parsedbf(buffer);
+        const fields = Object.keys(parsedDBF[0]);
+
+        document.querySelector('#fields').innerText = fields.join(', ');
+    })
+});
